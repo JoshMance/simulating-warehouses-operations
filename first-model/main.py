d1a3d9b434833e972fa942f7fd9f3bb41b38b3d7
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2023-12-29 19:05:47 +0800

    Creating customizable 2d array maps of layouts

---
+++ first-model/main.py
@@ -0,0 +1,8 @@
+import numpy as np                  # Arrays
+import random                       # Random number generation 
+import matplotlib.pyplot as plt     # General visualization
+import networkx as nx               # Graph visualization 
+from libpysal.weights import lat2W  #  Grid adjacency matrix creator
+
+from tqdm import tqdm
+import itertools
