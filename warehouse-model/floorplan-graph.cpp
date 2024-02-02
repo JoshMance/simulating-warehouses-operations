c5941e4e60be2b187c9e02c27cf59a483c2eeab2
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2024-02-02 08:09:09 +0800

    Creating all location objects

--- warehouse-model/floorplan-graph.cpp
+++ warehouse-model/floorplan-graph.cpp
@@ -11,39 +11,99 @@
 
 #include <iostream>
 #include <string.h>
-#include <valarray> // USed for element-wise operations on arrays
+#include <valarray>     // USed for element-wise operations on arrays
+#include <list>
 using namespace std;
 
+class Agent;
+class Ticket;
+class Location;
 
-valarray<int> SHELF_SIZE {2, 2};    // Row and column dimensions of a shelf
-valarray<int> GAP_SIZE {1, 2};       // Row and column distances between shelves
-valarray<int> NUM_SHELVES {3, 2};   // The number of shelves along any given row and column
+class Location {        // Holds each discrete location in a floor plan
+    public:             // Access specifier
+        int row;
+        int col;
+        Ticket* tickets[12];
+        Location* adj_locations[4];
+
+        Location(){} 
+
+        Location(int row, int col) {
+            row = row;
+            col = col;
+
+        }
+};
+
+class Agent {          // An employee who picks products from shelves
+    public:
+        int id;
+        Location* location;
+};
+
+class Ticket {          // A product that can be picked
+    public:
+        int number;
+        Location* locations[12];
+};
+
+valarray<int> SHELF_SIZE {1, 1};    // Row and column dimensions of a shelf
+valarray<int> GAP_SIZE {1, 1};       // Row and column distances between shelves
+valarray<int> NUM_SHELVES {2, 2};   // The number of shelves along any given row and column
 
 // Calculating the required row and column dimensions
 valarray<int> GRID_SIZE = NUM_SHELVES*(GAP_SIZE + SHELF_SIZE) + GAP_SIZE;
-
 int GRID_AREA = GRID_SIZE[0]*GRID_SIZE[1];
+
+int SHELF_AREA = NUM_SHELVES[0]*NUM_SHELVES[1]*SHELF_SIZE[0]*SHELF_SIZE[1];
+int NUM_LOCATIONS = GRID_AREA - SHELF_AREA;
+
+list<Ticket> tickets;
+list<Location> locations;
 
 int main() {
 
-    // Creating a grid floor plan and putting 1s at shelf locations and 
-    // 0s elsewhere
+    // Creating a grid floor plan. Putting 1s at shelf locations and 0s elsewhere.
     int grid[GRID_SIZE[0]][GAP_SIZE[1]];
     memset(grid, 0, sizeof(grid));
 
     for (int row = 0; row < GRID_SIZE[0]; row++) {
         bool shelf_in_row = (row % (GAP_SIZE[0] + SHELF_SIZE[0])) >= GAP_SIZE[0];
-
+        
         for (int col = 0; col < GRID_SIZE[1]; col++) {
              bool shelf_in_col = (col % (GAP_SIZE[1] + SHELF_SIZE[1])) >= GAP_SIZE[1];
             
             if (shelf_in_row && shelf_in_col) {
-                cout << "here!" << row << " " << col << endl;
+             
                 grid[row][col] = 1;
+    
             }
         }
     }
 
-    cout << "Initialization Complete"; 
+    // Running loop again - fix this inefficiency moving forward
+    for (int row = 0; row < GRID_SIZE[0]; row++) {
+        bool shelf_in_row = (row % (GAP_SIZE[0] + SHELF_SIZE[0])) >= GAP_SIZE[0];
+        
+        for (int col = 0; col < GRID_SIZE[1]; col++) {
+             bool shelf_in_col = (col % (GAP_SIZE[1] + SHELF_SIZE[1])) >= GAP_SIZE[1];
+            
+            if (shelf_in_row && shelf_in_col) {
+             
+                Location new_location;
+                new_location.row = row;
+                new_location.col = col;
+                locations.push_back(new_location);
+
+            }
+        }
+    }
+
+    // Checking all locations
+    for (list<Location>::iterator it = locations.begin(); it != locations.end(); ++it){
+        cout << it->row << " " << it->col << endl;
+    }
+
+
     return 0;
 } 
